4e3115932cc74261bb35c6ea4d4ec179746a7157
tobias.zeising@aditu.de <tobias.zeising@aditu.de@c0b6e06c-7496-11de-ba67-499d525147dd>
2009-09-02 16:14:12 +0000

    - Bugfix: error message output failure - Bugfix: priority reset after adding a feed - Version: 0.1 beta 2

--- trunk/application/Bootstrap.php
+++ trunk/application/Bootstrap.php
@@ -14,7 +14,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
      *
      * @var string
      */
-    public $version = "0.1 beta 1d";
+    public $version = "0.1 beta 2";
     
     
     /**
