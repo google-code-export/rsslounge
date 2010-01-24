ba51435487bf5c4544fb15b3f47fd5c9aa1645a4
tobias.zeising@aditu.de <tobias.zeising@aditu.de@c0b6e06c-7496-11de-ba67-499d525147dd>
2010-01-24 17:59:50 +0000

    - Bugfix: get correct database version - update wideimage from 1.0 beta 2 to 9.09.04

--- library/rsslounge/autoloader.php
+++ library/rsslounge/autoloader.php
@@ -17,20 +17,8 @@ class rsslounge_autoloader implements Zend_Loader_Autoloader_Interface {
      * @param string $class current classname
      */
     public function autoload($class) {
-        if($class=='wiImage')
-            require_once(Zend_Registry::get('config')->includePaths->library . '/wideimage/Image.class.php');
-        if($class=='wiTrueColorImage')
-            require_once(Zend_Registry::get('config')->includePaths->library . '/wideimage/TrueColorImage.class.php');
-        if($class=='wiPaletteImage')
-            require_once(Zend_Registry::get('config')->includePaths->library . '/wideimage/PaletteImage.class.php');
-        if($class=='wiException')
-            require_once(Zend_Registry::get('config')->includePaths->library . '/wideimage/Exception.class.php');
-        if($class=='wiDimension')
-            require_once(Zend_Registry::get('config')->includePaths->library . '/wideimage/Dimension.class.php');
-        if($class=='wiOpFactory')
-            require_once(Zend_Registry::get('config')->includePaths->library . '/wideimage/OpFactory.class.php');
-        if($class=='wiFileMapperFactory')
-            require_once(Zend_Registry::get('config')->includePaths->library . '/wideimage/FileMapperFactory.class.php');
+        if($class=='WideImage')
+            require_once(Zend_Registry::get('config')->includePaths->library . '/WideImage/WideImage.php');
     }
     
 }
